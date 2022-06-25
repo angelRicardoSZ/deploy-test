793452e39d6ba6a8593718f69b54bff8ccaa199d
angelRicardoSZ <angel.sanchezzefe@alumno.buap.mx>
2022-06-25 10:28:11 -0500

    data validation added

--- routes/products.js
+++ routes/products.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var productsController = require("../controllers/products")
+const validatorHandler = require("../middlewares/validator.handler")
+const {createProductSchema,updateProductSchema,getProductSchema} = require("../schemas/product.schema")
+
 
 /* GET products listing. */
 router.get('/',productsController.list);
 
-router.post('/new',productsController.create);
+router.post('/new', validatorHandler(createProductSchema,'body'),productsController.create);
 
-router.get('/detail/:id', productsController.detail);
+router.get('/detail/:id', validatorHandler(getProductSchema,'params'), productsController.detail);
 
-router.patch('/detail/:id',productsController.update);
+router.patch('/detail/:id',validatorHandler(getProductSchema,'params'), validatorHandler(updateProductSchema,'body'),productsController.update);
 
 router.delete('/delete/:id', productsController.delete);
 
